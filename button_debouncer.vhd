f878e43806213d728580fb263a082031ad4a492c
Markus Fröschle <mfro@mubf.de>
2022-12-26 10:19:16 +0100

    only allow powers of two as wait time
    code rounds up generics two next larger power of two to reduce
    resource cosumption. Experimental.

--- button_debouncer.vhd
+++ button_debouncer.vhd
@@ -23,19 +23,32 @@ entity button_debouncer is
 end entity button_debouncer;
 
 architecture rtl of button_debouncer is
-    subtype counter_type is natural range 0 to REQ_TICKS_STABLE - 1;
+    function next_power_of2(number : natural) return natural is
+        variable n : natural := number;
+        variable x : natural := 1;
+	begin
+        while n > 0 loop
+            n := n / 2;
+            x := x * 2;
+        end loop;
+        return x;
+    end function next_power_of2;
+	
+    constant t : natural := next_power_of2(REQ_TICKS_STABLE);
+    subtype counter_type is natural range 0 to t - 1;
     signal counter      : counter_type;
     signal btn_history  : std_ulogic_vector(SYNCHRONIZER_WIDTH downto 0) := (others => '0');
 begin
     p_debounce : process(all)
     begin
+        assert false report "REQ_TICKS_STABLE=" & natural'image(REQ_TICKS_STABLE) & " t=" & natural'image(t) severity note;
         if rising_edge(clk) then
             btn_history <= btn_history(btn_history'left - 1 downto btn_history'right) & button_in;
             if btn_history(btn_history'left) /= btn_history(btn_history'left - 1) then
                 -- button changed, reset counter to beginning
                 counter <= 0;
             else
-                counter <= counter + 1;
+                counter <= to_integer(to_unsigned(counter + 1, 32) and to_unsigned(counter_type'right, 32));
             end if;
             
             if counter = counter_type'right - 1 then            -- finished counting
